0e5b26ac43646fcddc0fce2e075c59b83579dff9
Nelson Garcia <ngarcia@ipac.caltech.edu>
2019-01-24 11:31:56 -0800

    Add MushROMs AboutDialog
    This class opens an "About" dialog box for MushROMs.

---
+++ src/MushROMs/AboutDialog.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="AboutDialog.cs" company="Public Domain">
+//     Copyright (c) 2019 Nelson Garcia. All rights reserved. Licensed under
+//     GNU Affero General Public License. See LICENSE in project root for full
+//     license information, or visit https://www.gnu.org/licenses/#AGPL
+// </copyright>
+
+namespace Maseya.MushROMs
+{
+    using System.Windows.Forms;
+
+    public partial class AboutDialog : Form
+    {
+        public AboutDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
